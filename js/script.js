73f3d762aae7a4484862f86a7a7c3caba2b9739a
Lukass <lukassh@gmail.com>
2019-03-03 14:10:49 +0100

    Fix script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = document.getElementById('countries');
 
+
 document.getElementById('search').addEventListener('click', searchCountries);
 
-function searchCountries() {
-    var countryName = document.getElementById('country-name').value;
-};
-
-function searchCountries() {
-    var countryName = document.getElementById('country-name').value;
-    if(!countryName.length) countryName = 'Poland';
-};
 
 function searchCountries() {
     var countryName = document.getElementById('country-name').value;
@@ -22,18 +15,22 @@ function searchCountries() {
         .then(showCountriesList);
 };
 
-function showCountriesList(resp) {
-  countriesList.innerHTML = '';
-}
+ 
 
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
-    resp.forEach(function(item) {
-        resp.forEach(function(item){
-		    var liEl = document.createElement('li');
-		    liEl.innerText = (item.name + ', Stolica :' +item.capital);
-		    countriesList.appendChild(liEl);
-		});//Here is the code that will execute on each successive item in the collection. A single item is hidden under an item variable.
+    resp.forEach(function(item){
+	    var liEl = document.createElement('li');
+	    liEl.innerHTML = '<header> ' +item.name +' </header>';
+	   	liEl.innerHTML += '<div class="col"><p> Region: </p><p>' +item.region +' </p></div>';
+	   	liEl.innerHTML += '<div class="col"><p> Capital </p><p>' +item.capital +' </p></div>';
+	   	liEl.innerHTML += '<div class="col"><p> Native name: </p><p>' +item.nativeName +' </p></div>';
+	   	liEl.innerHTML += '<div class="col"><p> Land area </p><p>' +item.area +' </p></div>';
+	   	liEl.innerHTML += '<div class="col"><p> Population </p><p>' +item.population +' </p></div>';
+	   	liEl.innerHTML += '<div class="col"><p> Language </p><p>' +item.demonym +' </p></div>';
+	   	liEl.innerHTML += '<div class="col"><p> Currency </p><p>' +item.currencies +' </p></div>';
+
+	    countriesList.appendChild(liEl);
     });
 }
 
